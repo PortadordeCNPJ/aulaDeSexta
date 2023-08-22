30423c7070db9ceda055124c34ee9d6050501e0e
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-08-22 11:11:50 -0300

    INNER JOIN

--- listar-usuario.php
+++ listar-usuario.php
@@ -1,11 +1,22 @@
 <h1>Listar Usuario</h1>
 <?php
-$sql_clientes = "SELECT * FROM tb_clientes";
-
+$sql_clientes = "SELECT * FROM tb_clientes AS id_cidade 
+                 INNER JOIN tb_cidades AS nome_cidade
+                 ON id_cidade.tb_cidades_nome_cidade = nome_cidade.nome_cidade";
 
 $res = $conn->query($sql_clientes);
-while ($row = $res->fetch_object()) {
-    # code...
+
+$qtd = $res->num_rows;
+
+if ($qtd > 0) {
+
+    while ($row = $res->fetch_object()) {
+        print "<tr>";
+        print "<td>" . $row->id . "</td>";
+        print "<td>" . $row->tb_cidades_nome_cidade . "</td>";
+        print "<td>" . $row->nome . "</td>";
+        print "<td>" . $row->email . "</td>";
+        print "<td>" . $row->data_nasc . "</td>";
+    }
 }
 ?>
-
